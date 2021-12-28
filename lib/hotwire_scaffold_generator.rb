562f1b3a1989ad4e66c3d2fa15c1cd3f4291c1d4
Chase Covington <mchasecov@gmail.com>
2021-12-28 13:09:09 -0500

    initial commit

---
+++ lib/hotwire_scaffold_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hotwire_scaffold_generator/version"
+
+module HotwireScaffoldGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
